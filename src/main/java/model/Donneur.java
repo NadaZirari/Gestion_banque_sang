725aeea944d93587ec6b3e4fa5705c9374336144
Nada Zirari <zirarinada20@gmail.com>
2025-10-09 14:31:47 +0100

    ajouter la classe donneur dans le model

--- src/main/java/model/Donneur.java
+++ src/main/java/model/Donneur.java
@@ -1,5 +1,46 @@
 package model;
 
+import java.time.LocalDate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "donneurs")
 public class Donneur {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private String cin;
+    private String telephone;
+    private LocalDate dateNaissance;
+    private double poids;
+    private String sexe;
+
+    @Enumerated(EnumType.STRING)
+    private GroupeSanguin groupeSanguin;
+
+    @Enumerated(EnumType.STRING)
+    private StatutDisponibilite statut;
+
+    @ManyToOne
+    @JoinColumn(name = "receveur_id")
+    private Receveur receveur;
+
+    public Donneur() {}
+
+    public void determinerEligibilite() {
+        int age = LocalDate.now().getYear() - dateNaissance.getYear();
+        if (age < 18 || age > 65 || poids < 50) {
+            this.statut = StatutDisponibilite.NON_ELIGIBLE;
+        } else {
+            this.statut = StatutDisponibilite.DISPONIBLE;
+        }
+    }
+
+    // apres j vais remplir g +S
+    
 }
