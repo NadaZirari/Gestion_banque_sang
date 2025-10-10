814f2938ad249611c831ca7814a762e398cd4bd7
Nada Zirari <zirarinada20@gmail.com>
2025-10-10 11:25:33 +0100

    ajouter limplementation du Receveurservice dans le package service

--- src/main/java/service/ReceveurService.java
+++ src/main/java/service/ReceveurService.java
@@ -1,5 +1,33 @@
 package service;
 
+
+
+import impl.ReceveurDaoImpl;
+import model.Receveur;
+
+import java.util.List;
+
 public class ReceveurService {
 
+    private ReceveurDaoImpl receveurDao = new ReceveurDaoImpl();
+
+    public void ajouterReceveur(Receveur receveur) {
+        receveurDao.save(receveur);
+    }
+
+    public List<Receveur> listerReceveurs() {
+        return receveurDao.findByPriorite();
+    }
+
+    public void supprimerReceveur(Long id) {
+        receveurDao.delete(id);
+    }
+
+    public Receveur chercherReceveur(Long id) {
+        return receveurDao.findById(id);
+    }
+
+    public void modifierReceveur(Receveur receveur) {
+        receveurDao.update(receveur);
+    }
 }
