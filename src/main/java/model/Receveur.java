b8efa0fbb16db98888a391a2942f1a75c6d0bce7
Nada Zirari <zirarinada20@gmail.com>
2025-10-10 09:45:24 +0100

    ajouter controller of receveur

--- src/main/java/model/Receveur.java
+++ src/main/java/model/Receveur.java
@@ -1,5 +1,42 @@
 package model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "receveurs")
 public class Receveur {
 
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    private String cin;
+    private String telephone;
+    private String sexe;
+
+    @Enumerated(EnumType.STRING)
+    private GroupeSanguin groupeSanguin;
+
+    private String situation; 
+
+    @Enumerated(EnumType.STRING)
+    private StatutReceveur statut = StatutReceveur.EN_ATTENTE;
+
+    @OneToMany(mappedBy = "receveur")
+    private List<Donneur> donneurs;
+
+    // Méthode de mise à jour du statut
+    public void majStatut() {
+        int nbPoches = donneurs != null ? donneurs.size() : 0;
+        if ((situation.equals("NORMAL") && nbPoches >= 1)
+         || (situation.equals("URGENT") && nbPoches >= 3)
+         || (situation.equals("CRITIQUE") && nbPoches >= 4)) {
+            this.statut = StatutReceveur.SATISFAIT;
+        }
+    }
+
+   }
+
