5f79921130269e8e01d653318042be9310282ae5
Nada Zirari <zirarinada20@gmail.com>
2025-10-10 10:07:36 +0100

    importer qlq modifications sur le model

--- src/main/java/model/Receveur.java
+++ src/main/java/model/Receveur.java
@@ -34,9 +34,91 @@ public class Receveur {
         if ((situation.equals("NORMAL") && nbPoches >= 1)
          || (situation.equals("URGENT") && nbPoches >= 3)
          || (situation.equals("CRITIQUE") && nbPoches >= 4)) {
-            this.statut = StatutReceveur.SATISFAIT;
+            this.statut = StatutReceveur.TRAITE;
         }
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getCin() {
+		return cin;
+	}
+
+	public void setCin(String cin) {
+		this.cin = cin;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getSexe() {
+		return sexe;
+	}
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+
+	public GroupeSanguin getGroupeSanguin() {
+		return groupeSanguin;
+	}
+
+	public void setGroupeSanguin(GroupeSanguin groupeSanguin) {
+		this.groupeSanguin = groupeSanguin;
+	}
+
+	public String getSituation() {
+		return situation;
+	}
+
+	public void setSituation(String situation) {
+		this.situation = situation;
+	}
+
+	public StatutReceveur getStatut() {
+		return statut;
+	}
+
+	public void setStatut(StatutReceveur statut) {
+		this.statut = statut;
+	}
+
+	public List<Donneur> getDonneurs() {
+		return donneurs;
+	}
+
+	public void setDonneurs(List<Donneur> donneurs) {
+		this.donneurs = donneurs;
+	}
+    
+    
 
    }
 
