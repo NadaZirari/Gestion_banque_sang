fb43cd8a815f99cc8051b83bed2b91a831a5f3b3
Nada Zirari <zirarinada20@gmail.com>
2025-10-10 10:14:47 +0100

    ajouter implementation du classe receveur DAO

--- src/main/java/impl/ReceveurDaoImpl.java
+++ src/main/java/impl/ReceveurDaoImpl.java
@@ -1,5 +1,58 @@
 package impl;
 
-public class ReceveurDaoImpl {
+//package dao.impl;
 
+import dao.ReceveurDao;
+import model.Receveur;
+import javax.persistence.*;
+import java.util.List;
+
+public class ReceveurDaoImpl implements ReceveurDao {
+
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("bloodbankPU");
+    private EntityManager em = emf.createEntityManager();
+
+    @Override
+    public void save(Receveur receveur) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(receveur);
+        tx.commit();
+    }
+
+    @Override
+    public Receveur findById(Long id) {
+        return em.find(Receveur.class, id);
+    }
+
+    @Override
+    public List<Receveur> findAll() {
+        return em.createQuery("SELECT r FROM Receveur r", Receveur.class).getResultList();
+    }
+
+    @Override
+    public void update(Receveur receveur) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.merge(receveur);
+        tx.commit();
+    }
+
+    @Override
+    public void delete(Long id) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Receveur receveur = em.find(Receveur.class, id);
+        if (receveur != null) {
+            em.remove(receveur);
+        }
+        tx.commit();
+    }
+
+    @Override
+    public List<Receveur> findByPriorite() {
+        return em.createQuery(
+                "SELECT r FROM Receveur r ORDER BY r.situationMedicale DESC",
+                Receveur.class).getResultList();
+    }
 }
